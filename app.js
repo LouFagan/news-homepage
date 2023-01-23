6336b718b705dc2fab82c8a31da3414de0d69cac
LouFagan <lfagan@pixelmein.ca>
2023-01-23 14:23:32 -0500

    clean up

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const menu = document.querySelector("menu");
 const menuOpen = document.querySelector(".menu-open");
 const menuClose = document.querySelector(".menu-close");
 const navBar = document.querySelector(".navbar");
 const navbarMobile = document.querySelector(".navbar__mobile");
-const header = document.querySelector(".header");
-const main = document.querySelector(".main");
 const overlay = document.querySelector(".overlay");
 
 menuOpen.addEventListener("click", function () {
-  navBar.style.display = "flex";
   navbarMobile.style.display = "flex";
   menuClose.style.display = "flex";
   menuOpen.style.display = "none";
